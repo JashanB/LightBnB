f572e083d28c29d5df0de097d8ddb9c4a3eabc8f
Jashan <jashan_brar50@hotmail.com>
2020-01-08 19:56:24 +0000

    add queries 1

---
+++ 1_queries/3_list_by_city.sql
@@ -0,0 +1,10 @@
+SELECT properties.*, avg(rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+WHERE city LIKE '%ancouve%'
+GROUP BY properties.id
+HAVING avg(rating) >= 4
+ORDER BY cost_per_night
+LIMIT 10;
+
+
